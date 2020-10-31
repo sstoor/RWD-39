51288e7ea3f915900347f278261cde6fed23517e
tgravy <tom.graving@live.com>
2020-10-31 19:03:35 +1000

    error page style
    removed duplicate styling from homepage
    
    stylised error page
    
    models.py indent formatting

--- auction/models.py
+++ auction/models.py
@@ -5,13 +5,15 @@ from . import db
 
 db = SQLAlchemy()
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, nullable=False)
-    emailid = db.Column(db.String(100), index=True, nullable=False )
+    emailid = db.Column(db.String(100), index=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     comments = db.relationship('Comment', backref='user')
+
 
 class Watch(db.Model):
     __tablename__ = 'watches'
@@ -22,13 +24,13 @@ class Watch(db.Model):
     comments = db.relationship('Comment', backref='watch')
     bids = db.relationship('Bid', backref='watch')
 
-    
-    def __repr__(self): #string print method
-        return "<Name: {}>".format(self.name) 
+    def __repr__(self):  # string print method
+        return "<Name: {}>".format(self.name)
+
 
 class Itemcreate(db.Model):
     __tablename__ = 'Items'
-    id =  db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     make = db.Column(db.String(10))
     model = db.Column(db.String(10))
     movement = db.Column(db.String(10))
@@ -38,16 +40,17 @@ class Itemcreate(db.Model):
     year = db.Column(db.String(10))
     condition = db.Column(db.String(10))
 
+
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
     created_at = db.Column(db.DateTime, default=datetime.now())
-    #add the foreign keys
+    # add the foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     destination_id = db.Column(db.Integer,
-    db.ForeignKey('watches.id'))
-     
+                               db.ForeignKey('watches.id'))
+
     def __repr__(self):
         return "<Comment: {}>".format(self.text)
 
@@ -57,11 +60,10 @@ class Bid(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
     created_at = db.Column(db.DateTime, default=datetime.now())
-    #add the foreign keys
+    # add the foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     destination_id = db.Column(db.Integer,
-    db.ForeignKey('watches.id'))
-
+                               db.ForeignKey('watches.id'))
 
     def __repr__(self):
         return "<Bid: {}>".format(self.text)
